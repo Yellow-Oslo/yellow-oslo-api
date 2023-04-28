bdea5a1df4d492ff5e388991c3d47eb20a5deba7
Jacob Clark <clark.3289@osu.edu>
2023-04-28 14:27:33 -0400

    Test for Api done

---
+++ tests/Yellow.Oslo.Domain.Tests/ItemTests.cs
@@ -0,0 +1,34 @@
+using Yellow.Oslo.Domain.Catalog;
+
+namespace Yellow.Oslo.Domain.Tests;
+
+[TestClass]
+public class ItemTests
+{
+  [TestMethod]
+public void Can_Create_New_Item()
+{
+    var item = new Item("Name", "Description", "Brand", 10.00m);
+
+    Assert.AreEqual("Name", item.Name);
+    Assert.AreEqual("Description", item.Description);
+    Assert.AreEqual("Brand", item.Brand);
+    Assert.AreEqual(10.00m, item.Price);
+}
+  [TestMethod]
+public void Can_Create_Add_Rating()
+{
+    // Arrange
+    var item = new Item("Name", "Description", "Brand", 10.00m);
+    var rating = new Rating(5, "Name", "Review");
+
+    // Act
+    item.AddRating(rating);
+
+    // Assert
+    Assert.AreEqual(rating, item.Ratings[0]);
+}
+
+
+
+}
