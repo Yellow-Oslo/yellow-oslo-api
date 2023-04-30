66b4116840f1ecef7b5d726a0c8f00234b36315e
Jacob Clark <clark.3289@osu.edu>
2023-04-30 12:16:06 -0400

    Securing API

--- Yellow.Oslo.Api/Program.cs
+++ Yellow.Oslo.Api/Program.cs
@@ -1,16 +1,42 @@
 using Yellow.Oslo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
-
-
+using Yellow.Oslo.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+string authority = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentNullException("Autho0:Authority");
+
+string audience = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentNullException("Autho0:Authority");
+
 
 // Add services to the container.
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options => 
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => 
+{
+    options.Authority = authority;
+    options.Audience = audience;
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("delete:catalog", policy =>
+        policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));
+});
+
+
 builder.Services.AddDbContext<StoreContext>(options => 
 options.UseSqlite("Data Source= ../Registrar.sqlite", 
 b => b.MigrationsAssembly("Yellow.Oslo.Api")));
@@ -40,7 +66,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseCors();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.UseAuthorization();
 
